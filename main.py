692cc668ccedba0d836e633778f8f97f0d9ad0d5
ST19008 <79234190+ST19008@users.noreply.github.com>
2021-07-21 21:55:48 +1200

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 app = Tk()
 app.title("math thing")
 
+def try_again():
+   try_again.1=random.choice(num)
+   try_again.2 = random.choice(num)
+   thing=label(app, text="test")
+    
+# def submit?
+
+# def result
+
 app.geometry("300x300")
 start = Button(app, text="Start", command=try_again)
+start.place(relx=0.5, rely=1)
