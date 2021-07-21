94b302c1817a61cede4fdccdb8bada96432731aa
ST19008 <79234190+ST19008@users.noreply.github.com>
2021-07-21 20:20:37 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import random
 from tkinter import *
 
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+app = Tk()
+app.title("math thing")
+
+app.geometry("300x300")
+start = Button(app, text="Start", command=try_again)
